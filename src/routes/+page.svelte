079bdf16ac43a0955aed6946d233dba46e349a53
Dan Calacci <dcalacci@media.mit.edu>
2024-09-17 21:53:46 -0400

    using exif to adjust dpi...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import * as piexif from 'piexifjs';
+
 	let stream: MediaStream | null = null;
 	let videoElement;
 	let canvasElement;
@@ -82,25 +84,21 @@
 		const canvasElement = document.createElement('canvas');
 		canvasElement.width = videoElement.videoWidth;
 		canvasElement.height = videoElement.videoHeight;
-		canvasElement.getContext('2d')?.drawImage(videoElement, 0, 0);
-
-		try {
-			const blob = await new Promise<Blob>((resolve, reject) => {
-				canvasElement.toBlob((b) => (b ? resolve(b) : reject('Blob is null')), 'image/jpeg', 0.8);
-			});
-			photoBlob = blob;
-			photoUrl = URL.createObjectURL(blob);
-			isPhotoTaken = true;
-			isCameraActive = false;
+		const ctx = canvasElement.getContext('2d');
+		if (ctx) {
+			ctx.drawImage(videoElement, 0, 0);
+			processImage(canvasElement);
 
 			if (canShare) {
 				try {
-					const file = new File([blob], 'signature.jpg', { type: 'image/jpeg' });
-					await navigator.share({
-						files: [file],
-						title: 'Save Signature',
-						text: 'Please save this signature image to your device.'
-					});
+					if (photoBlob) {
+						const file = new File([photoBlob], 'signature.jpg', { type: 'image/jpeg' });
+						await navigator.share({
+							files: [file],
+							title: 'Save Signature',
+							text: 'Please save this signature image to your device.'
+						});
+					}
 				} catch (error) {
 					console.error('Error sharing:', error);
 					// Fallback to download if sharing fails
@@ -109,9 +107,8 @@
 			} else {
 				downloadSignature();
 			}
-		} catch (error) {
-			console.error('Error capturing signature:', error);
-			alert('Failed to capture signature. Please try again.');
+		} else {
+			console.error('Failed to get 2D context for canvas');
 		}
 	}
 
@@ -131,86 +128,133 @@
 		const scaledCanvas = document.createElement('canvas');
 		const ctx = scaledCanvas.getContext('2d');
 
-		scaledCanvas.width = canvas.width * scaleFactor;
-		scaledCanvas.height = canvas.height * scaleFactor;
+		scaledCanvas.width = Math.round(canvas.width * scaleFactor);
+		scaledCanvas.height = Math.round(canvas.height * scaleFactor);
 
 		if (ctx) {
+			ctx.imageSmoothingEnabled = true;
+			ctx.imageSmoothingQuality = 'high';
 			ctx.drawImage(canvas, 0, 0, scaledCanvas.width, scaledCanvas.height);
-			photoUrl = scaledCanvas.toDataURL('image/png');
+
+			const imageData = scaledCanvas.toDataURL('image/jpeg', 0.95);
+
+            const initialExifObj = {"0th": {}, "Exif": {}, "GPS": {}, "Interop": {}, "1st": {}, "thumbnail": null};
+            const exifBytes = piexif.dump(initialExifObj);
+			const insertedImageData = piexif.insert(exifBytes, imageData);
+			const rawImageData = atob(insertedImageData.split(',')[1]);
+			const array = new Uint8Array(rawImageData.length);
+
+			for(let i = 0; i < rawImageData.length; i++) {
+				array[i] = rawImageData.charCodeAt(i);
+			}
+
+			const blob = new Blob([array], {type: 'image/jpeg'});
+			photoBlob = blob;
+			photoUrl = URL.createObjectURL(blob);
+			isPhotoTaken = true;
+			isCameraActive = false;
+
+			// Set DPI to 75
+			const dpi = 75;
+			const dpiHex = dpi.toString(16).padStart(4, '0');
+			const xResolution = '0x' + dpiHex + '000100000000';
+			const yResolution = '0x' + dpiHex + '000100000000';
+			
+			const zeroth: { [key: number]: any } = {};
+			zeroth[piexif.ImageIFD.XResolution] = xResolution;
+			zeroth[piexif.ImageIFD.YResolution] = yResolution;
+			zeroth[piexif.ImageIFD.ResolutionUnit] = 2; // inches
+
+			const exifObj = {"0th": zeroth, "Exif": {}, "GPS": {}, "Interop": {}, "1st": {}, "thumbnail": null};
+			const exifStr = piexif.dump(exifObj);
+
+			const newImageData = piexif.insert(exifStr, imageData);
+			const newBlob = dataURItoBlob(newImageData);
+			photoBlob = newBlob;
+			photoUrl = URL.createObjectURL(newBlob);
 		} else {
 			console.error('Failed to get 2D context');
 		}
-		isPhotoTaken = true;
-		isCameraActive = false;
+	}
+
+	function dataURItoBlob(dataURI: string) {
+		const byteString = atob(dataURI.split(',')[1]);
+		const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+		const ab = new ArrayBuffer(byteString.length);
+		const ia = new Uint8Array(ab);
+		for (let i = 0; i < byteString.length; i++) {
+			ia[i] = byteString.charCodeAt(i);
+		}
+		return new Blob([ab], {type: mimeString});
 	}
 </script>
 
 <main class="container mx-auto p-4 flex flex-col items-center">
 	<div class="max-w-md w-full">
-		{#if hasDriversLicense === null}
-			<h2 class="text-xl mb-4 text-center">Do you have a PA driver's license?</h2>
-			<div class="flex justify-center gap-4">
-				<button on:click={() => handleDriversLicenseResponse(true)} class="btn btn-primary"
-					>Yes</button
-				>
-				<button on:click={() => handleDriversLicenseResponse(false)} class="btn btn-primary"
-					>No</button
-				>
-			</div>
-		{:else if hasDriversLicense}
-			<p class="mb-4 text-center">
-				Great! You can proceed directly to the PA voter registration application.
-			</p>
-			<div class="flex justify-center">
-				<button on:click={goToPARegistration} class="btn btn-success">Go to PA Registration</button>
-			</div>
-		{:else}
-			<p class="mb-4 text-center">
-				PA requires a photo of your signature. Take a photo here first and save it to your device
-				before heading to the registration site.
-			</p>
-			<div class="flex justify-center">
-				{#if !isCameraActive}
-					{#if !isPhotoTaken}
-						<button on:click={openCamera} class="btn btn-primary mb-4">Open Camera</button>
+			{#if hasDriversLicense === null}
+				<h2 class="text-xl mb-4 text-center">Do you have a PA driver's license?</h2>
+				<div class="flex justify-center gap-4">
+					<button on:click={() => handleDriversLicenseResponse(true)} class="btn btn-primary"
+						>Yes</button
+					>
+					<button on:click={() => handleDriversLicenseResponse(false)} class="btn btn-primary"
+						>No</button
+					>
+				</div>
+			{:else if hasDriversLicense}
+				<p class="mb-4 text-center">
+					Great! You can proceed directly to the PA voter registration application.
+				</p>
+				<div class="flex justify-center">
+					<button on:click={goToPARegistration} class="btn btn-success">Go to PA Registration</button>
+				</div>
+			{:else}
+				<p class="mb-4 text-center">
+					PA requires a photo of your signature. Take a photo here first and save it to your device
+					before heading to the registration site.
+				</p>
+				<div class="flex justify-center">
+					{#if !isCameraActive}
+						{#if !isPhotoTaken}
+							<button on:click={openCamera} class="btn btn-primary mb-4">Open Camera</button>
+						{/if}
+					{:else}
+						<div class="flex-col items-center justify-center">
+							<video id="camera" class="w-full mb-4" autoplay playsinline>
+								<track kind="captions" src="" label="Captions" />
+							</video>
+							<div class="flex justify-center">
+								<button on:click={saveSignature} class="btn btn-outline btn-primary">
+									Capture Signature
+								</button>
+							</div>
+						</div>
 					{/if}
-				{:else}
+				</div>
+				{#if isPhotoTaken}
 					<div class="flex-col items-center justify-center">
-						<video id="camera" class="w-full mb-4" autoplay playsinline>
-							<track kind="captions" src="" label="Captions" />
-						</video>
-						<div class="flex justify-center">
-							<button on:click={saveSignature} class="btn btn-outline btn-primary">
-								Capture Signature
-							</button>
+						<img src={photoUrl} alt="Signature" class="w-full rounded-lg border-4 border-primary" />
+
+						<div class="card-body">
+							<h2 class="card-title text-success bg-primary text-primary-content p-2 italic">Signature captured successfully!</h2>
+							<p class="text-sm text-base-content opacity-70">
+								{canShare
+									? "Your signature should have been saved to your device automatically. You're ready to continue to PA's voter registration site!"
+									: 'Please save this image to your device before proceeding.'}
+							</p>
+							<div class="flex flex-col justify-center mt-4 gap-4">
+								<button on:click={clearSignature} class="btn btn-outline btn-error"
+									>Clear and Retake</button
+								>
+								<button on:click={saveSignature} class="btn btn-primary">Save to Device</button>
+								<button on:click={goToPARegistration} class="btn btn-success"
+									>Go to PA Registration</button
+								>
+							</div>
 						</div>
 					</div>
 				{/if}
-			</div>
-			{#if isPhotoTaken}
-				<div class="flex-col items-center justify-center">
-					<img src={photoUrl} alt="Signature" class="w-full rounded-lg border-4 border-primary" />
-
-					<div class="card-body">
-						<h2 class="card-title text-success bg-primary text-primary-content p-2 italic">Signature captured successfully!</h2>
-						<p class="text-sm text-base-content opacity-70">
-							{canShare
-								? "Your signature should have been saved to your device automatically. You're ready to continue to PA's voter registration site!"
-								: 'Please save this image to your device before proceeding.'}
-						</p>
-						<div class="flex flex-col justify-center mt-4 gap-4">
-							<button on:click={clearSignature} class="btn btn-outline btn-error"
-								>Clear and Retake</button
-							>
-							<button on:click={downloadSignature} class="btn btn-primary">Save to Device</button>
-							<button on:click={goToPARegistration} class="btn btn-success"
-								>Go to PA Registration</button
-							>
-						</div>
-					</div>
-				</div>
 			{/if}
-		{/if}
 	</div>
 </main>
 
